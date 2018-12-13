6aeb2e6ecf12f196ad329648a12cfba642f5a4aa
shufo <meikyowise@gmail.com>
2018-12-13 10:20:54 +0900

    Bump to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExDocRefined.MixProject do
   def project do
     [
       app: :ex_doc_refined,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env() == :prod,
